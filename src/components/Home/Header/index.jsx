43c3104193fd267b603740e05e8e0fb335e7cae0
mohammad niroujazi <mohammad.nj@jacobb.ai>
2023-01-04 13:07:29 -0500

    changed header and not found image

--- src/components/Home/Header/index.jsx
+++ src/components/Home/Header/index.jsx
@@ -3,9 +3,9 @@
 
  const Header = () => (
      <header className='home-header'>
-        <h2>Programming with Mohamad </h2>
+        <h2>Technology & Programming </h2>
         <h1>
-        <span>"</span>Blog<span>"</span>
+        <span>"</span>with Moh<span>"</span>
         </h1> 
         <p>
         Awesome place to make oneself <br/> productive and entertained through weekly programming updates.
--- src/components/common/EmptyList/index.jsx
+++ src/components/common/EmptyList/index.jsx
@@ -6,7 +6,7 @@ import './styles.css';
 export function EmptyList() {
   return (
     <div className='emptyList-wrap'>
-      <img src='/assets/images/13525-empty.gif' alt='empty' />
+      <img src='assets/images/13525-empty.gif' alt='empty' />
     </div>
   );
 }
