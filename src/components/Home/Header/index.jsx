33d00e43dea852ca2044cbfdaad3f515c0704870
mohammad niroujazi <mohammad.nj@jacobb.ai>
2023-01-04 13:13:27 -0500

    updated header

--- src/components/Home/Header/index.jsx
+++ src/components/Home/Header/index.jsx
@@ -8,7 +8,7 @@
         <span>"</span>with Moh<span>"</span>
         </h1> 
         <p>
-        Awesome place to make oneself <br/> productive and entertained through weekly programming updates.
+        Awesome place to make oneself <br/> productive and entertained through weekly programming and technology updates.
         </p>
      </header>
    );
