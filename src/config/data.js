7eb48cc40da97e7390f1c7370d12563d4e6a3658
mohammad niroujazi <mohammad.nj@jacobb.ai>
2023-01-04 12:54:48 -0500

    updated data file

--- src/config/data.js
+++ src/config/data.js
@@ -5,7 +5,7 @@ export const blogList = [
       category: 'development',
       subCategory: ['frontend', 'ui/ux', 'design'],
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "CSS is the stylesheet language that every web developer must know. It allows us to make web pages more appealing to the user. A web page without CSS is like a human without clothes. CSS has a lot of cool features that allow us to create amazing experiences on the web. By using this stylesheet language, you can make web pages beautiful and responsive on all devices. You can also create good-looking layouts with the power of flexbox and grid. There is a lot to do with CSS and it's very useful for web development. However, writing and managing good CSS code is not that easy. Especially if you are working on large projects and you are not using a CSS framework. That's why it would be useful to use some available CSS tools on the web to make your life easier. In this article, I'm going to share with you 7 CSS tools that you should be using. These tools will help you to write better CSS code and make your life easier. Let's get started. 1. Get Waves. 2. PurgeCSS. 3. CSS Scan. 4. CSS Grid Generator. 5. CSS Gradient. 6. CSS Stats. 7. CSS Minifier.",
       authorName: 'Mohamad Niroujazi',
       authorAvatar: 'assets/images/author.jpg',
       createdAt: 'Nov 20 , 2022',
@@ -15,10 +15,10 @@ export const blogList = [
     {
       id: 2,
       title: 'How to become a professional programmer',
-      category: 'travel',
-      subCategory: ['vacation', 'holidays', 'sight seeing'],
+      category: 'development',
+      subCategory: ['programming', 'coding', 'work'],
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Want to get started in the world of coding and build websites as a career? First, learn programming concepts. Then learn about object-orientation programming. Learn to code in JavaScript. Build a basic website using only HTML. After that, Design websites using CSS.",
       authorName: 'Mohamad Niroujazi',
       authorAvatar: 'assets/images/author.jpg',
       createdAt: 'Nov 20 , 2022',
@@ -28,10 +28,10 @@ export const blogList = [
     {
       id: 3,
       title: 'Learn Python with practical examples',
-      category: 'shopping',
-      subCategory: ['e-commerce store', 'clothing', 'shopping store'],
+      category: 'development',
+      subCategory: ['programming', 'coding', 'work'],
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Python is an interpreted, high-level and general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant indentation.",
       authorName: 'Mohamad Niroujazi',
       authorAvatar: 'assets/images/author.jpg',
       createdAt: 'Nov 20 , 2022',
@@ -41,10 +41,10 @@ export const blogList = [
     {
       id: 4,
       title: 'Arificial Intelligence: The Future of the World',
-      category: 'adventure',
-      subCategory: ['adrenaline', 'stay-fit', 'lifestyle'],
+      category: 'development',
+      subCategory: ['programming', 'coding', 'work'],
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Artificial intelligence is the simulation of human intelligence processes by machines, especially computer systems. Specific applications of AI include expert systems, natural language processing, speech recognition and machine vision.",
       authorName: 'Mohamad Niroujazi',
       authorAvatar: 'assets/images/author.jpg',
       createdAt: 'Nov 20 , 2022',
@@ -54,10 +54,10 @@ export const blogList = [
     {
       id: 5,
       title: 'What Is Computer Vision?',
-      category: 'cooking',
-      subCategory: ['bbq', 'food', 'lifestyle'],
+      category: 'technology',
+      subCategory: ['programming', 'coding', 'work'],
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "Computer vision is a field of artificial intelligence (AI) that enables computers and systems to derive meaningful information from digital images, videos and other visual inputs — and take actions or make recommendations based on that information. If AI enables computers to think, computer vision enables them to see, observe and understand. Computer vision works much the same as human vision, except humans have a head start. Human sight has the advantage of lifetimes of context to train how to tell objects apart, how far away they are, whether they are moving and whether there is something wrong in an image.",
       authorName: 'Mohamad Niroujazi',
       authorAvatar: 'assets/images/author.jpg',
       createdAt: 'Nov 20 , 2022',
@@ -67,11 +67,11 @@ export const blogList = [
     {
       id: 6,
       title: 'Developer Roadmap',
-      category: 'travel',
-      subCategory: ['beaches', 'sea', 'holidays'],
+      category: 'development',
+      subCategory: ['programming', 'coding', 'work'],
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      authorName: 'Mohamad Niroujazie',
+        "Want to get started in the world of coding and build websites as a career? First, learn programming concepts. Then learn about object-orientation programming. Learn to code in JavaScript. Build a basic website using only HTML. After that, Design websites using CSS.  ",
+      authorName: 'Mohamad Niroujazi',
       authorAvatar: 'assets/images/author.jpg',
       createdAt: 'Nov 20 , 2022',
       cover:
@@ -82,10 +82,10 @@ export const blogList = [
     {
       id: 7,
       title: 'Learn JavaScript with practical examples',
-      category: 'art',
-      subCategory: ['skill', 'design', 'passion'],
+      category: 'development',
+      subCategory: ['programming', 'coding', 'work'],
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "JavaScript is a programming language that adds interactivity to your website. This happens in games, in the behavior of responses when buttons are pressed or with data entry on forms; with dynamic styling; with animation, etc. JavaScript is versatile and beginner-friendly. With more experience, you'll be able to create games, animated 2D and 3D graphics, comprehensive database-driven apps, and much more. JavaScript itself is relatively compact, yet very flexible. Developers have written a variety of tools on top of the core JavaScript language, unlocking a vast amount of functionality with minimum effort.",
       authorName: 'Mohamad Niroujazi',
       authorAvatar: 'assets/images/author.jpg',
       createdAt: 'Nov 20 , 2022',
