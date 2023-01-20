d6c6ca9b17cf06b20b7721602aadce281dcff13c
mohammad niroujazi <mohammad.nj@jacobb.ai>
2023-01-19 23:56:40 -0500

    added logo

--- src/components/Home/Header/index.jsx
+++ src/components/Home/Header/index.jsx
@@ -1,8 +1,12 @@
  import React from 'react'
  import './styles.css'
-
+import logo from './logo_01.jpg'
  const Header = () => (
      <header className='home-header'>
+     <div className='home-header__logo'>
+        <img 
+       className="logo" src={logo} alt='logo..' />
+      </div>
         <h2>Technology & Programming </h2>
         <h1>
         <span>"</span>with Moh<span>"</span>
